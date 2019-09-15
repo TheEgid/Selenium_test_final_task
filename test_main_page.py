2a19b8bc4eb3df6bd06d8dd5f4cf9aea47cb2080
TheEgid <egid.f1@gmail.com>
2019-09-15 23:02:14 +0300

    Realize LoginPage

--- test_main_page.py
+++ test_main_page.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 import pytest
 
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 
@@ -18,16 +19,23 @@ def browser():
     browser.quit()
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+@pytest.fixture
+def link():
+    yield 'http://selenium1py.pythonanywhere.com/'
+
+
+def test_guest_can_go_to_login_page(browser, link):
     page = MainPage(browser, link)
     page.open()
-    time.sleep(2)
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+def test_guest_should_see_login_link(browser, link):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+
