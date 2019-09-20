c2940133d809acf7cf57f73014989298680d7669
TheEgid <egid.f1@gmail.com>
2019-09-20 21:12:42 +0300

    Ensure sovereignty testы from contents

--- test_product_page.py
+++ test_product_page.py
@@ -9,14 +9,23 @@ import pytest
 from pages.product_page import ProductPage
 
 
-product_base_link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
+PRODUCT_BASE_LINK = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
 
 
-@pytest.fixture
-def link():
-    yield product_base_link
-    #yield 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+# @pytest.yield_fixture
+# def link():
+#yield 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+
+
+urls = [
+    f"{PRODUCT_BASE_LINK}/?promo=offer0", f"{PRODUCT_BASE_LINK}/?promo=offer1",
+    f"{PRODUCT_BASE_LINK}/?promo=offer2", f"{PRODUCT_BASE_LINK}/?promo=offer3",
+    f"{PRODUCT_BASE_LINK}/?promo=offer4", f"{PRODUCT_BASE_LINK}/?promo=offer5",
+    f"{PRODUCT_BASE_LINK}/?promo=offer6",
+    pytest.param(f"{PRODUCT_BASE_LINK}/?promo=offer7",
+                 marks=pytest.mark.xfail),
+    f"{PRODUCT_BASE_LINK}/?promo=offer8", f"{PRODUCT_BASE_LINK}/?promo=offer9"
+]
 
 
 @pytest.mark.parametrize('link', urls)
@@ -24,16 +33,17 @@ def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    browser.implicitly_wait(5)
+
+    browser.implicitly_wait(3)
     page.solve_quiz_and_get_code()
+    # time.sleep(30)
+    browser.implicitly_wait(3)
+
     page.should_be_add_product_to_basket()
     page.should_prices_equal()
     page.should_products_equal()
-    browser.implicitly_wait(5)
+    browser.implicitly_wait(3)
 
-
-
-
-
+    # time.sleep(15)
 
 
