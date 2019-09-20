14f440b7d38b2d75959c79d1070a9386e2cfaefd
TheEgid <egid.f1@gmail.com>
2019-09-21 00:24:57 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -13,15 +13,19 @@ class LoginPageLocators():
 class ProductPageLocators:
     BTN_ADD_TO_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
 
-    MSG_ADD_TO_BASKET = (By.CSS_SELECTOR, "div.alert:nth-child(1)")
+    MSG_ADD_TO_BASKET = (By.CSS_SELECTOR, "div.alertinner strong")
 
-    #ALERT_ADDED_TO_CART = (By.CSS_SELECTOR, "div.alertinner")
-    # ALERT_CART_STATUS = (By.CSS_SELECTOR, ".alert-noicon.alert-info p")
-    # ALERT_ADDED_TO_CART = (By.XPATH, '//*[@id="messages"]/div[1]/div/strong')
-    PRODUCT_NAME = (By.TAG_NAME, "h1")
+    BTN_OPEN_BASKET = (By.CSS_SELECTOR, "div.basket-mini.pull-right.hidden-xs > span > a")
 
-    BASKET_PRODUCT_PRICE = (By.CSS_SELECTOR, ".basket-mini")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
 
-    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color:nth-child(2)")
+    BASKET_PRODUCT = (By.CSS_SELECTOR, 'h3 > a')
+
+    BASKET_PRODUCT_PRICE = (By.XPATH, '// *[ @ id = "messages"] / div[3] / div / p[1] / strong')
+
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > .alert-success")
+
 
 
